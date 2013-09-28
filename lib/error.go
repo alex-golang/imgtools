7bb18cccaa6badab6c08cee6447c1864d8f692a8
Jim Teeuwen <jimteeuwen@gmail.com>
2013-09-28 17:42:37 +0200

    Revises ccode comments in lib/error.go

--- lib/error.go
+++ lib/error.go
@@ -18,8 +18,19 @@ func Check(err error) {
 //
 // This simply wraps the defer/recover mechanic.
 // Any errors which occur in the executed code are expected
-// to panic. This makes code easier to write, as it does
-// not have to check errors everywhere.
+// to throw a panic. This makes code easier to follow, as it
+// can ommit the error handling code everywhere.
+//
+// For instance, the following functions (f1, f2, f3) may
+// perform any operation that throws a panic when an error occurs.
+// The panic is turned into an error, returned by `lib.With`.
+//
+//    err := lib.With(func() {
+//        f1()
+//        f2()
+//        f3()
+//    })
+//
 func With(f func()) (err error) {
 	defer func() {
 		if x := recover(); x != nil {
