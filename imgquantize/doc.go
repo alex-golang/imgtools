a1f7563baaf23478dcca5a85ec578681b085c3aa
Jim Teeuwen <jimteeuwen@gmail.com>
2013-09-29 00:06:18 +0200

    Removes some pointless files.

--- imgquantize/doc.go
+++
@@ -1,8 +0,0 @@
-// This file is subject to a 1-clause BSD license.
-// Its contents can be found in the enclosed LICENSE file.
-
-/*
-imgquantize quantizes the input image and yields the results.
-It allows one to specify one of the support quantization algorithms.
-*/
-package main
--- imgscale/doc.go
+++
@@ -1,8 +0,0 @@
-// This file is subject to a 1-clause BSD license.
-// Its contents can be found in the enclosed LICENSE file.
-
-/*
-imgscale resizes the given image to the specified dimensions
-and using the given scaling algorithm.
-*/
-package main
