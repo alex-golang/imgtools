8efc23f827babe52bf20629bf71402bbad2c40ce
Jim Teeuwen <jimteeuwen@gmail.com>
2013-10-02 17:18:39 +0200

    Adds imghash example script to testdata directory.

---
+++ testdata/imghash.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Test if two given images are to be considered equal.
+# This works even if they have different sizes and aspect ratios.
+
+A="original.png"
+B="original_thumb.png"
+
+HASHA=$(imghash -hash average $A)
+HASHB=$(imghash -hash average $B)
+
+DISTANCE=$(imghash -diff $HASHA $HASHB)
+EPSILON=3
+
+if [ $DISTANCE -le $EPSILON ]; then
+	echo "$A and $B are equal" # Images are equal.
+else
+	echo "$A and $B are not equal" # Images are not equal.
+fi
+
+
